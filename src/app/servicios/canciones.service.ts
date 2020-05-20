0a64d49713ddcae99d7850fa81bb6def65f16536
enrique-bit <62971062+enrique-bit@users.noreply.github.com>
2020-05-20 00:55:06 -0500

    Canciones Servicio

---
+++ src/app/servicios/canciones.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CancionesService {
+
+  constructor(private http: HttpClient) { }
+
+  traerCanciones() {
+    return this.http.get(`${environment.API_URL}/canciones`);
+  }
+
+  eliminarCancion(id) {
+    return this.http.delete(`${environment.API_URL}/canciones/${id}`);
+  }
+}
